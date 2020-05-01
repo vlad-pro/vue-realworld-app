8c138000fddc8673d57e602dfdce3775ff5bc9a8
vproskurin <vladimir.proskurin@gmail.com>
2020-04-30 23:48:34 -0700

    Starting with vuex

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,9 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: { user: 'abc123', name: 'Player1'}
+  },
   mutations: {},
   actions: {},
   modules: {}
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,13 +1,18 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create Event, {{ userName }}</h1>
+    <p>This event was created by {{ userName }}</p>
+  </div>
 </template>
 
 <script>
 export default {
-
+  computed: {
+    userName() {
+      return this.$store.state.user.name
+    }
+  }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
