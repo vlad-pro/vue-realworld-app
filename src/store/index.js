ae2a9cc0d2085c178e59a1cf6d6a9bec4344b0e3
vproskurin <vladimir.proskurin@gmail.com>
2020-05-04 00:06:36 -0700

    map getters added and tried out

--- src/store/index.js
+++ src/store/index.js
@@ -14,9 +14,35 @@ export default new Vuex.Store({
       'education',
       'food',
       'community'
+    ],
+    todos: [
+      { id: 1, text: '...', done: true},
+      { id: 2, text: '...', done: false},
+      { id: 3, text: '...', done: true},
+      { id: 4, text: '...', done: false}
+    ],
+    events: [
+      { id: 1, title: '...', organizer: '...'},
+      { id: 2, title: '...', organizer: '...'},
+      { id: 3, title: '...', organizer: '...'},
+      { id: 4, title: '...', organizer: '...'}
     ]
   },
   mutations: {},
   actions: {},
-  modules: {}
+  modules: {},
+  getters: {
+    catLength: state => {
+      return state.categories.length
+    },
+    doneTodos: state => {
+      return state.todos.filter(todo => todo.done)
+    },
+    activeTodosCount: (state, getters) => {
+      return state.todos.length - getters.doneTodos.length
+    },
+    getEventById: state => id => {
+      return state.events.find(event => event.id === id)
+    }
+  }
 });
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -2,17 +2,29 @@
   <div>
     <h1>Create Event, {{ user.name }}</h1>
     <p>This event was created by {{ user.id }}</p>
+    <p>There are {{ catLength }} categories</p>
     <ul>
       <li v-for="cat in categories" :key="cat">{{ cat }}</li>
     </ul>
+    <p>{{ getEvent(1) }}</p>
+    <p>{{ getEventById(3) }}</p>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState, mapGetters} from 'vuex'
 
 export default {
-  computed: mapState(['user','categories'])
+  computed: {
+    getEvent() {
+      return this.$store.getters.getEventById
+    },
+    catLength() {
+      return this.$store.getters.catLength
+    },
+    ...mapState(['user', 'categories']),
+    ...mapGetters(['getEventById'])
+  }
 }
 </script>
 
