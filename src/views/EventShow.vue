59255b65c01618ded8719ba914a951031a9af766
vproskurin <vladimir.proskurin@gmail.com>
2020-05-18 11:52:29 -0700

    Adding progress bar to properly display an event, so that event page is loaded simultaniously, i.e. data and the event format page.
    Do not forget to simulate a delay (-d 1500 for json server)

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -35,17 +35,22 @@
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex'
+import { mapState } from 'vuex'
+import NProgress from 'nprogress'
+import store from '@/store/index.js'
 
 export default {
   props: ['id'],
-  created() {
-    this.fetchEvent(this.id)
+  beforeRouteEnter(routeTo, routeFrom, next) {
+    NProgress.start()
+    store.dispatch('event/fetchEvent', routeTo.params.id).then(() => {
+      NProgress.done()
+      next()
+    })
   },
   computed: mapState({
     event: state => state.event.event
-  }),
-  methods: mapActions('event', ['fetchEvent'])
+  })
 }
 </script>
 
