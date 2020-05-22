74f84c8ecadfb0e6d5ed23a63fe2ece926f44b84
vproskurin <vladimir.proskurin@gmail.com>
2020-05-22 13:29:23 -0700

    Leftovers since last commit

---
+++ src/views/NetworkIssue.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Oh, it's not working</h1>
+    <h3>
+      You might have a network issue, please click the back button and try again
+    </h3>
+    <router-link :to="{ name: 'event-list' }">Take me back</router-link>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Oh, it's not here.</h1>
+    <h3>This is not the {{ resource }} you are looking for.</h3>
+    <router-link :to="{ name: 'event-list' }">Take me back</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    resource: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
